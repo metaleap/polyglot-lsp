6707cd778510050c458d053c4c8fa0c1e23ed1d2
Phil Schumann <metaleap@users.noreply.github.com>
2023-03-05 02:09:27 +0100

    [autopush] «How come we never talk anymore?»

--- lang_csharp/lsp_v3.17/types_enums.cs
+++ lang_csharp/lsp_v3.17/types_enums.cs
@@ -1,4 +1,4 @@
-// Language Server Protocol (LSP) v3.17 SDK for C#: auto-generated via github.com/metaleap/polyglot-vsx-and-lsp/gen/cmd/gen_lsp
+/// <summary>Language Server Protocol (LSP) v3.17 SDK for C#: auto-generated via github.com/metaleap/polyglot-vsx-and-lsp/gen/cmd/gen_lsp</summary>
 namespace lsp;
 
 
