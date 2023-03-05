5378b595b80e8ed9f305bd8c7533664111177610
Phil Schumann <metaleap@users.noreply.github.com>
2023-03-05 02:19:26 +0100

    [autopush] «Eschew obfuscation.»

--- lang_csharp/lsp_v3.17/types.cs
+++ lang_csharp/lsp_v3.17/types.cs
@@ -1,8 +1,8 @@
 /// <summary>Language Server Protocol (LSP) v3.17 SDK for C#: auto-generated via github.com/metaleap/polyglot-vsx-and-lsp/gen/cmd/gen_lsp</summary>
 namespace lsp;
 
-using Uinteger = System.UInt32;
 using String = System.String;
 using Integer = System.Int32;
+using Uinteger = System.UInt32;
 
 
